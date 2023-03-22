9e2764602f9764b9189058053cba58902a28b118
Jeffrey Sievers <20220020@mywhitecliffe.com>
2023-03-23 12:11:06 +1300

    trying to add function to ticket systetm

--- Code Folder/System.py
+++ Code Folder/System.py
@@ -1,31 +1,35 @@
 import Ticketing 
-print("\n\n ========== TICKET SYSTEM ==========\n\n")
+
+print(Ticketing.bcolors.OKBLUE + "\n\n ========== TICKET SYSTEM ==========\n\n" + Ticketing.bcolors.ENDC)
 
 Ticketing.menu()
 print("\n")
 
-selection = int(input("Enter your option: "))
+selection = int(input(Ticketing.bcolors.OKBLUE + "Enter your option: " + Ticketing.bcolors.ENDC))
 
-while selection != 0:
+
+
+while selection != 5:
     if selection == 1: 
         #preform option 1
         print()
-        print("1 called")
+        Ticketing.option1()
+        
         
     elif selection == 2:
         #do option 2
         print()
-        print("2 called")
+        
         
     elif selection == 3:
         #do option 3
         print()
-        print("3 called")
+        Ticketing.option3()
         
     elif selection == 4:
         #do option 4
         print()
-        print("4 called")
+        Ticketing.option4()
         
     else:
         print()
@@ -35,31 +39,6 @@ while selection != 0:
     Ticketing.menu()
     print("\n")
 
-    selection = int(input("Enter your option: "))
+    selection = int(input(Ticketing.bcolors.OKBLUE + "Enter your option: " + Ticketing.bcolors.ENDC))
 
 print("system shutdown")
-# Tickets can be submitted by providing all of the following information:
-# Staff ID,
-# Ticket creator name,
-# Contact email
-# Description of the issue
-
-
-# Statistics:
-
-# There should be a way to keep track of:
-# The number of tickets submitted
-# The number of resolved tickets
-# The Number of open tickets
-# A way to display those statistics to the console.
-
-
-# Displaying the ticket:
-
-# There should be a way to display the ticket information:
-# Ticket number,
-# Name of the ticket’s creator,
-# StaffID,
-# Email address,
-# Description of the issue,
-# Response from the IT department and ticket status (open, closed or reopened).
--- Code Folder/Ticketing.py
+++ Code Folder/Ticketing.py
@@ -1,12 +1,71 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+#  print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+
+
 def menu():
-    print("[1] option 1")
-    print("[2] option 2")
-    print("[3] option 3")
-    print("[4] option 4")
-    print("[5] Exit program")
+    print("[1] Display all tickets.")
+    print("[2] Submit new ticket.")
+    print("[3] provide response to ticket.")
+    print("[4] reopen ticket.")
+    print("[5] Display Statistics.")
+    print("[0] Exit.")
 
 
-author = "jeffrey"
+# class Ticket():
+#     Ticket_Number = None
+#     Ticket_Creator = None
+#     Staff_ID = None
+#     Email = None
+#     Description = None
+#     Response = None
+#     Ticket_Status = None
 
-if __name__ == "__main__":
-    menu()
+#     def __init__(self, Ticket_Number, Ticket_Creator, Staff_ID, Email, Description, Response, Ticket_Status):
+#         self.Ticket_Number = Ticket_Number
+#         self.Ticket_Creator = Ticket_Creator
+#         self.Staff_ID = Staff_ID
+#         self.Email = Email
+#         self.Description = Description
+#         self.Response = Response
+#         self.Ticket_Status = Ticket_Status
+
+#     def option2():
+#         print(bcolors.OKBLUE + "========== TICKET SYSTEM ==========" + bcolors.ENDC)
+#         Ticket_Number = input(print("Ticket_Number:", ))
+#         Ticket_Creator = input(print("Ticket_Creator:",))
+#         Staff_ID = input(print("Staff_ID:", ))
+#         Email = input(print("Email:", ))
+#         Description = input(print("Description:", ))
+#         Response = input(print("Response:", ))
+#         Ticket_Status = input(print("Ticket_Status:", ))
+
+
+def option1():
+    print("this option1 was made with a function.")
+
+
+# def option2():
+#     print("this option2 was made with a function.")
+
+
+def option3():
+    print("this option3 was made with a function.")
+
+
+def option4():
+    print("this option4 was made with a function.")
+
+
+# author = "jeffrey"
+
+# # if __name__ == "__main__":
+# #     menu()
