ccc51c5b7229af52310fed648242231915d87f56
Jeffrey Sievers <20220020@mywhitecliffe.com>
2023-03-30 10:14:43 +1300

    vrbiubrf

--- tests/testing.py
+++ tests/testing.py
@@ -74,3 +74,53 @@ def main():
 
 if __name__ == '__main__':
     
+
+    main
+
+    print("\n")
+
+    selection = int(input(bcolors.OKBLUE +"Enter your option: " + bcolors.ENDC))
+                    
+    while selection != 0:
+        if selection == 1:
+            # Display all tickets.
+            print("hitiing display")
+            
+            menu()
+            
+
+        elif selection == 2:
+            # creating new tickets
+            ts.CreatingTicket()
+            menu()
+            ticket_count+=1
+            
+
+        elif selection == 3:
+            # rovide response to ticket.
+            print()
+            
+
+        elif selection == 4:
+            # reopen ticket.
+            print()
+            
+
+        elif selection == 5:
+            # Display Statistics.
+            print()
+            
+
+        else:
+            print()
+            print("Invalid option. Choose from [0-5]")
+            menu()
+            
+
+        # print("\n")
+        # Ticketing.menu()
+        # print("\n")
+
+        selection = int(input(bcolors.OKBLUE +"Enter your option: " + bcolors.ENDC))
+                        
+    print("system shutdown")
