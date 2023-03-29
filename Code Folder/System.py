91c7ed02e965a410872f6a94931a1c84d374c941
FloodedCandle <floodedcandle26@gmail.com>
2023-03-29 20:07:44 +1300

    ticket done?

--- Code Folder/System.py
+++ Code Folder/System.py
@@ -1,24 +1,33 @@
 from Ticketing import *
 
 
+def menu():
+    print(bcolors.OKBLUE + "\n ========== TICKET SYSTEM ==========\n" + bcolors.ENDC)
+    print("[1] Display all tickets.")
+    print("[2] Submit new ticket.")
+    print("[3] rovide response to ticket.p")
+    print("[4] reopen ticket.")
+    print("[5] Display Statistics.")
+    print("[0] Exit.")
+
 if __name__ == "__main__":
-    # Tickets = []
+    Tickets = []
     ticket_count = 2000
     ticket_amount = 0
 
-    ts = TicketSystem()
+    
 
     menu()
 
     print("\n")
 
-    selection = int(input(bcolors.OKBLUE +"Enter your option: " + bcolors.ENDC))
+    selection = int(input(bcolors.OKBLUE + "Enter your option: " + bcolors.ENDC))
                     
     while selection != 0:
         if selection == 1:
             # Display all tickets.
             print("hitiing display")
-            
+            Ticket.tickets
             menu()
             
 
--- learning pie again/Lesson2_Python_Variables.py
+++ learning pie again/Lesson2_Python_Variables.py
@@ -25,3 +25,59 @@ x = str(3) # will be "3"
 y = int(3) # will be 3
 z = float(3) # will be 3.0
 
+"GET THe TYPe"
+# You can get the data typ of a variable with the:
+type() #function.
+#Example
+x = 5
+y = "me"
+print(type(x))
+print(type(y))
+
+"SINGLE OR DOUBLE QUOTES?"
+#String variables can be declared either by using single or double quotes:
+x = "me"
+#the same as 
+x = 'me'
+
+"CASE=SENSITIVE"
+#variable names are case-sensitive
+#Example
+a = 4
+A = "papi"
+#A will not overwrite a
+
+"VARIABLE NAMES"
+#a variable can have short name(like x and y) or a more descriptive name(age, name, total_colume). 
+# Rules for python variable:
+# a variable name must start with a letter or the underscore character
+# a variable name cannot start with a number.
+# a variable name can only contain alpha-numeric characters and underscores(A-z, 0-9, and _)
+# variable names are case-sensitive(age, Age and AGE are three different variables)
+# a ariable name cannot be any of the python keywords.
+
+"LEGAL VARIABLE NAME EXAMPLE"
+myvar = 'jay'
+my_var = 'jay'
+my_var_ = 'jay'
+myVar = 'jay'
+MYVAR = 'jay'
+myvar2 = 'jay'
+
+"ILLEGAL VARIABLE NAMES EXAMPLE"
+2myvar = 'joe'
+my-var = 'joe'
+my var = 'joe'
+
+"MULTI WORD VARIABLE NAMES"
+#Variable names with more than one word can be iffivuil to read.
+#There are sveral techniques you can use to make them more readable:
+"CAMEL CASE"
+#Each word,  except the first,  starts with capital letter:
+myVariableName = 'tommy'
+"PASCAL CASE"
+#Each word starts with a capital letter:
+MyVariableName = 'tommy'
+"SNAKE CASE"
+#Easg ord is separated by an underscore character:
+my_variable_name = 'tommy'
